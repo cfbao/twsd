bfa2fbee59c1d4219cf641495ec0bc774ee03ae0
cfbao <cfbao.is@gmail.com>
2023-10-06 21:50:59 -0400

    Terraform skeleton

---
+++ terraform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_version = "~> 1.6"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.19"
+    }
+  }
+
+  backend "s3" {
+    region = "us-east-2"
+    bucket = "cfbao-terraform-state"
+    key    = "github/cfbao/twsd/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+
+  default_tags {
+    tags = {
+      Terraform = "https://github.com/cfbao/twsd"
+    }
+  }
+}
