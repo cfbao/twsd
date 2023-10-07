fa29071ac4dedba9e3782081f3ccea0360a589cf
cfbao <cfbao.is@gmail.com>
2023-10-07 17:35:58 -0400

    tuck away serialization

--- backend/Chenfeng.Twsd.Api/Program.cs
+++ backend/Chenfeng.Twsd.Api/Program.cs
@@ -1,38 +1,50 @@
-using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
 using Amazon.Lambda.RuntimeSupport;
 
-var outputStream = new MemoryStream();
+await LambdaBootstrapBuilder
+	.Create<APIGatewayHttpApiV2ProxyRequest, APIGatewayHttpApiV2ProxyResponse>(
+		Handler,
+		new LambdaSerializer())
+	.Build()
+	.RunAsync();
 
-var utf8 = new UTF8Encoding(encoderShouldEmitUTF8Identifier: false);
-
-await new LambdaBootstrap(Handler).RunAsync();
-
-Task<InvocationResponse> Handler(InvocationRequest invocation)
+static Task<APIGatewayHttpApiV2ProxyResponse> Handler(APIGatewayHttpApiV2ProxyRequest request)
 {
-	var request = JsonSerializer.Deserialize(invocation.InputStream, JsonContext.Default.APIGatewayHttpApiV2ProxyRequest);
-
 	string name = request?.RawPath.TrimStart('/') switch
 	{
 		{ Length: > 0 } path => path,
 		_ => "World",
 	};
 
-	var response = new APIGatewayHttpApiV2ProxyResponse
+	return Task.FromResult(new APIGatewayHttpApiV2ProxyResponse
 	{
 		StatusCode = 200,
 		Body = $"Hello, {name}!",
-	};
-
-	outputStream.SetLength(0);
-	JsonSerializer.Serialize(outputStream, response, JsonContext.Default.APIGatewayHttpApiV2ProxyResponse);
-	outputStream.Position = 0;
-	return Task.FromResult(new InvocationResponse(outputStream, disposeOutputStream: false));
+	});
 }
 
 [JsonSerializable(typeof(APIGatewayHttpApiV2ProxyRequest))]
 [JsonSerializable(typeof(APIGatewayHttpApiV2ProxyResponse))]
 [JsonSourceGenerationOptions(PropertyNameCaseInsensitive = true)]
 internal partial class JsonContext : JsonSerializerContext { }
+
+internal class LambdaSerializer : ILambdaSerializer
+{
+	public T Deserialize<T>(Stream requestStream)
+	{
+		return (T)(object)JsonSerializer.Deserialize(
+			requestStream,
+			JsonContext.Default.APIGatewayHttpApiV2ProxyRequest)!;
+	}
+
+	public void Serialize<T>(T response, Stream responseStream)
+	{
+		JsonSerializer.Serialize(
+			responseStream,
+			(APIGatewayHttpApiV2ProxyResponse)(object)response!,
+			JsonContext.Default.APIGatewayHttpApiV2ProxyResponse);
+	}
+}
