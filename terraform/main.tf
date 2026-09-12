c60596ae188943a5eb75da87dc01fbf5d526f3f5
Chenfeng Bao <chenfeng@chenfengbao.com>
2026-09-12 09:56:57 -0700

    tf touchup

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,17 +1,18 @@
 terraform {
-  required_version = "~> 1.6"
+  required_version = "~> 1.16"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.19"
+      version = "~> 6.0"
     }
   }
 
   backend "s3" {
-    region = "us-east-2"
-    bucket = "cfbao-terraform-state"
-    key    = "github/cfbao/twsd/terraform.tfstate"
+    region       = "us-east-2"
+    bucket       = "cfbao-terraform-state"
+    key          = "github/cfbao/twsd/terraform.tfstate"
+    use_lockfile = true
   }
 }
 
